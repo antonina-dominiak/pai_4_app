efb9d1fd9a65dc0b0e59f96c4e10ce6d81cd0555
antonina-dominiak <antonina.dominiak01@gmail.com>
2023-06-20 20:42:27 +0200

    tutorial v2

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
-  const subject = "React";
+  console.log(props);
+  const subject = props.subject;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        "Hello, {props.subject}!"
+        "Hello, {subject}!"
         </p>
       </header>
     </div>
